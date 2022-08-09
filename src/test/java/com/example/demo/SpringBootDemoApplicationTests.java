4a066fcf4cfdfc039f7597126f5f4aeef1e139a9
theartak <kartachok@gmail.com>
2022-08-09 12:04:34 +0400

    refactor

--- src/test/java/com/example/demo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/example/demo/SpringBootDemoApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringBootDemoApplicationTests {
 
 	@Test
 	public static void main(String[] args) {
